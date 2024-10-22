31a67aa9eda6892cf21ee6a8052b190cabc54446
James-Trema <andrdu29@live.fr>
2024-10-22 12:11:56 +0200

    up

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,7 +7,7 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
   if (authService.isLoggedIn()) {
-    return true;
+    return true;  // Autorisation d'accéder à la route
   } else {
     router.navigate(['/login']);  // Redirection vers la page login si l'utilisateur n'est pas authentifié
     return false;
