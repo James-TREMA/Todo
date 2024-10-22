fa315232436c843e67070c2eb3bf2064d00c38e5
James-Trema <andrdu29@live.fr>
2024-10-22 11:49:44 +0200

    up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],  // Ajout des modules ici
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  // Ajout de la logique pour gérer le login
+  constructor() {}
+
+  onLogin() {
+    // Logique de gestion de la soumission du formulaire
+    if (this.username === 'Pierre' && this.password === 'okand') {
+      // Redirection ou gestion de l'accès ici
+    } else {
+      this.errorMessage = 'Nom d\'utilisateur ou mot de passe incorrect';
+    }
+  }
 }
